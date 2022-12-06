a6f8d01b9ff6c847b6fa35a9d1d506317add5810
LevanPangani5 <levanfangani55@gmail.com>
2022-12-06 12:35:25 -0800

    add creat video

--- frontend/components/MainView.js
+++ frontend/components/MainView.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import UploadModal from './UploadModal'
+import Video from './Video'
+import BottomBar from './BottomBar'
 import styles from "../styles/MainView.module.css"
 import Signup from './Signup'
 import { useEffect , useState } from 'react'
@@ -8,6 +11,9 @@ import { getProgramInstance } from '../utils/utils'
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 
 import {useAccount} from '../hooks/useAccount'
+import {useTicktock} from '../hooks/useTicktock'
+
+
 
 const anchor = require('@project-serum/anchor')
 const utf8 = anchor.utils.bytes.utf8
@@ -26,13 +32,89 @@ const MainView = () => {
 
   const program = getProgramInstance(connection ,wallet)
 
+  const [ticktocks , setTickTocks] = useState()
+  const [newVideoShow, setNewVideoShow] = useState(false)
+  const[description, setDescription]= useState('')
+  const[videoUrl,setVideoUrl]= useState('')
+  const[userDetail, setUserDetail]= useState()
+
+
   const {signup}= useAccount()
+  const { getTicktocks , likeVideo , createComment , newVideo , getComments } = useTicktock(
+    setTickTocks,
+    userDetail,
+    videoUrl,
+    description,
+    setDescription,
+    setVideoUrl,
+    setNewVideoShow
+  )
+
+  useEffect(()=>{
+    if(wallet.connected){
+      checkAccount()
+      getTicktocks()
+    }
+  }, [wallet.connected])
+
+const checkAccount = async()=>{
+  let [user_pda] = await anchor.web3.PublicKey.findProgramAddressSync(
+    [utf8.encode('user'), wallet.publicKey.toBuffer()],
+    program.programId,
+  )
+  try{
+     const userInfo = await program.account.userAccount.fetch(user_pda)
+     console.log(userInfo)
+     setUserDetail(userInfo)
+     setAccount(true)
+  }
+  catch(e){
+setAccount(false)
+  }
+}
+
+
   return (
     <> 
     {
       isAccount ?(
         <div>
-          ticktock will go here
+          {newVideoShow&&(
+            <UploadModal
+            description ={description}
+            videoUrl={videoUrl}
+            newVideo={newVideo}
+            setDescription={setDescription}
+            setVideoUrl={setVideoUrl}
+            setNewVideoShow ={setNewVideoShow}
+            />
+          )}
+          <div classNmae={styles.appVideos}>
+            {ticktocks.length ===0 ?(
+              <h1>No Videos</h1>
+            ):(
+              ticktocks.map((ticktock, id)=>{
+                <Video
+                key={id}
+                address={ticktock.publicKey.toBase58()}
+                url={ticktock.account.videoUrl}
+                channel={ticktock.account.creatorName}
+                index={ticktock.account.index.toNumber()}
+                likes={ticktock.likes}
+                description={ticktock.account.description}
+                likeVideo={likeVideo}
+                likesAddress={ticktock.account.peopleWhoLiked}
+                createComment={createComment}
+                getComments={getComments}
+                commentCount={ticktock.account.commentCount.toNumber()}S
+                />
+              })
+            )}
+          </div>
+          <BottomBar
+          setNewVideoShow={setNewVideoShow}
+          getTicktocks={getTicktocks}
+          />
         </div>
       ):(
         <Signup signup= {signup} wallet={wallet.publicKey.toBase58}/>
